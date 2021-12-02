836aba7b8f68a752ce2370906758e4627309838f
hstehouwer <hstehouwer@jibes.nl>
2021-12-02 15:59:49 +0100

    refactor

--- day2.exs
+++ day2.exs
@@ -1,30 +1,32 @@
 defmodule DAY2 do
   def follow_course(instructions, do_aim \\ false, position \\ {0,0,0})
 
-  def follow_course(instructions, _do_aim, position) when [] == instructions do
+  def follow_course([], _do_aim, position) do
     position
   end
 
-  def follow_course([hd|tl], do_aim, {pos, depth, aim}) when do_aim do
-    case hd do
-      {"forward", amount} ->
-        follow_course(tl, do_aim, {pos+amount, depth+aim*amount, aim})
-      {"down", amount} ->
-        follow_course(tl, do_aim, {pos, depth, aim+amount})
-      {"up", amount} ->
-        follow_course(tl, do_aim, {pos, depth, aim-amount})
-    end
+  def follow_course([{"forward", amount}|tl], true, {pos, depth, aim}) do
+    follow_course(tl, true, {pos+amount, depth+aim*amount, aim})
   end
 
-  def follow_course([hd|tl], do_aim, {pos, depth, aim}) do
-    case hd do
-      {"forward", amount} ->
-        follow_course(tl, do_aim, {pos+amount, depth, aim})
-      {"down", amount} ->
-        follow_course(tl, do_aim, {pos, depth+amount, aim})
-      {"up", amount} ->
-        follow_course(tl, do_aim, {pos, depth-amount, aim})
-    end
+  def follow_course([{"forward", amount}|tl], false, {pos, depth, aim}) do
+    follow_course(tl, false, {pos+amount, depth, aim})
+  end
+
+  def follow_course([{"down", amount}|tl], true, {pos, depth, aim}) do
+    follow_course(tl, true, {pos, depth, aim+amount})
+  end
+
+  def follow_course([{"down", amount}|tl], false, {pos, depth, aim}) do
+    follow_course(tl, false, {pos, depth+amount, aim})
+  end
+
+  def follow_course([{"up", amount}|tl], true, {pos, depth, aim}) do
+    follow_course(tl, true, {pos, depth, aim-amount})
+  end
+
+  def follow_course([{"up", amount}|tl], false, {pos, depth, aim}) do
+    follow_course(tl, false, {pos, depth-amount, aim})
   end
 end
 
