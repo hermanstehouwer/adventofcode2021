bc94f0c4cc757ce6f03cb8ad92892437d8c0c18f
hstehouwer <hstehouwer@jibes.nl>
2021-12-03 14:39:43 +0100

    Some niceness

--- day3.exs
+++ day3.exs
@@ -2,7 +2,10 @@ defmodule DAY3 do
   @type binary_number :: list(integer)
   @type numlist :: list(integer)
 
+
+  @spec binary_number_to_int(binary_number)::integer
   def binary_number_to_int(number) do Enum.map(number, &Integer.to_string/1) |> Enum.join("") |> String.to_integer(2) end
+
 
   @spec reducer(list(binary_number), numlist()) :: numlist()
   def reducer(to_add, acc)
@@ -10,6 +13,7 @@ defmodule DAY3 do
   def reducer(to_add, acc) do
     to_add |> Enum.zip(acc)  |> Enum.map(& (elem(&1, 0) + elem(&1, 1)))
   end
+
 
   @spec gamma_and_epsilon(numlist(), integer())::{integer, integer}
   def gamma_and_epsilon(list, cutoff) do
@@ -19,13 +23,16 @@ defmodule DAY3 do
     {gamma, epsilon}
   end
 
+
   @spec sum_list(list(binary_number()))::numlist()
   def sum_list(list) do list |> Enum.reduce(List.duplicate(0, 13), &DAY3.reducer/2) end
+
 
   @spec apply_filter(list(binary_number()), integer(), integer(), boolean())::list(binary_number())
   def apply_filter(list, index, filter_value, most_common)
   def apply_filter(list, index, filter_value, true) do Enum.filter(list, fn x -> Enum.at(x, index) == filter_value end ) end
   def apply_filter(list, index, filter_value, false) do apply_filter(list, index, 1-filter_value, true) end
+
 
   @spec find_rating(list(binary_number), integer, boolean) :: integer
   def find_rating(list, index, most_common)
