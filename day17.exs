7fee6f91e53eedf694dd268a58e081739f6504c0
hstehouwer <hstehouwer@jibes.nl>
2021-12-17 18:52:35 +0100

    made_smaller

--- day17.exs
+++ day17.exs
@@ -9,12 +9,10 @@ IO.inspect round(y1*(y1-1)/2)
 
 #part 2:
 defmodule Shot do
-  defp update_x(0), do: 0
-  defp update_x(vx), do: vx + ((vx < 0 && 1) || -1)
-
   def in_target(xp,yp,_xv,_yv,x1,x2,y1,y2) when xp in x1..x2 and yp in y1..y2, do: true
   def in_target(xp,yp,_xv,_yv,_x1,x2,y1,_y2) when xp > x2 or yp < y1, do: false
-  def in_target(xp,yp,xv,yv,x1,x2,y1,y2), do: in_target(xp+xv, yp+yv, update_x(xv), yv-1, x1, x2, y1, y2)
+  def in_target(xp,yp,0,yv,x1,x2,y1,y2), do: in_target(xp, yp+yv, 0, yv-1, x1, x2, y1, y2)
+  def in_target(xp,yp,xv,yv,x1,x2,y1,y2), do: in_target(xp+xv, yp+yv, xv-1, yv-1, x1, x2, y1, y2)
 end
 
 for xv <- 1..x2 do
