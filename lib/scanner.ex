55c3438e873d261245c71f9495d47f8f1819116d
hstehouwer <hstehouwer@jibes.nl>
2021-12-20 00:23:48 +0100

    some refactor

--- lib/scanner.ex
+++ lib/scanner.ex
@@ -13,72 +13,52 @@ defmodule Scanner do
   def split_n_toi(x), do: String.split(x, ",", trim: true) |> Enum.map(&String.to_integer/1)
   def from_strings([_label | coords]), do: %Scanner{coords: Enum.map(coords, &split_n_toi/1)}
 
-  @spec turns()::[coord()]
-  defp turns(), do: [
-    [1,2,3], [-2,1,3], [-1,-2,3], [2,-1,3], [1, -3, 2], [3, 1, 2], [-1, 3, 2], [-3, -1, 2],
-    [1, 3, -2], [-3, 1, -2], [-3, -1, -2], [-1,-3, -2], [3,-1, -2], [1, -2, -3], [2, 1, -3],
-    [-1, 2, -3], [-2, -1, -3], [-3, 2, 1], [-2, -3, 1], [3, -2, 1], [2, 3, 1], [3, 2, -1],
-    [-2, 3, -1], [-3,-2,-1],[2,-3,-1]]
+  defp directions(), do: [[1,2,3], [-2,1,3], [-1,-2,3], [2,-1,3], [1, -3, 2], [3, 1, 2], [-1, 3, 2], [-3, -1, 2],
+    [1, 3, -2], [-3, 1, -2], [-3, -1, -2], [-1,-3, -2], [3,-1, -2], [1, -2, -3], [2, 1, -3],[-2, 3, -1],
+    [-1, 2, -3], [-2, -1, -3], [-3, 2, 1], [-2, -3, 1], [3, -2, 1], [2, 3, 1], [3, 2, -1],[-3,-2,-1],[2,-3,-1]]
 
-  @spec rot(coord(), coord())::coord()
-  defp rot(coord, [a,b,c]), do: [
-    Enum.at(coord, abs(a)-1) * div(a, abs(a)),
-    Enum.at(coord, abs(b)-1) * div(b, abs(b)),
-    Enum.at(coord, abs(c)-1) * div(c, abs(c))]
-
-  @spec rotate(Scanner.t(), coord())::Scanner.t()
-  def rotate(scanner, rotation), do: %Scanner{coords: Enum.map(scanner.coords, &(rot(&1, rotation))), rotation: rotation}
+  def rotate(%Scanner{coords: coords}, rotation), do: %Scanner{coords: Enum.map(coords, &(rotate(&1, rotation))), rotation: rotation}
+  def rotate(coord, [a,b,c]), do: [rotate(coord, a), rotate(coord, b), rotate(coord, c)]
+  def rotate(coord, a), do: Enum.at(coord, abs(a)-1) * div(a, abs(a))
 
   @spec rotations(Scanner.t())::[Scanner.t()]
-  def rotations(scanner), do: Enum.map(turns(), &(rotate(scanner, &1)) )
+  def rotations(scanner), do: Enum.map(directions(), &(rotate(scanner, &1)) )
 
   def dist([x1, x2, x3], [y1, y2, y3]), do: [x1-y1, x2-y2, x3-y3]
   def  mov([x1, x2, x3], [y1, y2, y3]), do: [x1+y1, x2+y2, x3+y3]
 
-  def aligned(s, tf) do
+  def find_alignment(s, tf) do
     shifts = for a <- s.coords, b <- tf.coords, do: dist(a, b)
     {most, freq} = Enum.frequencies(shifts) |> Enum.max_by(&(elem(&1,1)))
     if freq >= 12, do: most, else: nil
   end
 
-  def fits(aligned, to_fit) do
+  def find_fit(aligned, to_fit=%Scanner{}) do
     for tf <- Scanner.rotations(to_fit) do
-      for s <- aligned do
-        {aligned(s, tf), tf.rotation}
-      end
+      for s <- aligned, do: {find_alignment(s, tf), tf.rotation}
     end
     |> List.flatten()
     |> Enum.filter( &( !is_nil(elem(&1,0)) ) )
     |> List.first()
   end
 
-  def find_fit(aligned, to_fit)
-
-  def find_fit(_aligned, []), do: nil
-
   def find_fit(aligned, [hd|tl]) do
-    fit = fits(aligned, hd)
+    fit = find_fit(aligned, hd)
     case fit do
       nil -> find_fit(aligned, tl)
       _ -> {hd, elem(fit, 0), elem(fit, 1)} #fit == {distance, rotation}
     end
   end
 
-  @spec rot_and_mov(Scanner.t(), coord(), coord())::Scanner.t()
-  def rot_and_mov(fitting, distance, rotation) do
-    %Scanner{
-      coords: Enum.map(fitting.coords, &( mov(rot(&1, rotation), distance) )),
-      offset: distance,
-      rotation: rotation
-    }
-  end
+  def rotate_and_move(fitting, distance, rotation), do: %Scanner{offset: distance, rotation: rotation,
+      coords: Enum.map(fitting.coords, &( mov(rotate(&1, rotation), distance) ))}
 
   @spec reconstruct([Scanner.t()], [Scanner.t()])::[Scanner.t()]
   def reconstruct(aligned, []), do: aligned
   def reconstruct(aligned, to_align) do
     {fitting, distance, rotation} = find_fit(aligned, to_align)
     to_align = to_align -- [fitting]
-    aligned = [rot_and_mov(fitting, distance, rotation) |aligned]
+    aligned = [rotate_and_move(fitting, distance, rotation) |aligned]
     reconstruct(aligned, to_align)
   end
 
